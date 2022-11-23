82dcaccba8bcbaa43bb2e2de2ad052546a0f0891
Rubi <quipu1@naver.com>
2022-11-23 17:14:34 +0900

    메모 리스트 출력 구현

--- memo/src/main/java/memo/MemoService.java
+++ memo/src/main/java/memo/MemoService.java
@@ -2,4 +2,5 @@ package memo;
 
 public interface MemoService {
     void changeMemo(int id, int password, String newPost);
+    void showMemoList();
 }
--- memo/src/main/java/memo/repository/MemoRepository.java
+++ memo/src/main/java/memo/repository/MemoRepository.java
@@ -3,5 +3,6 @@ package memo.repository;
 import memo.Memo;
 
 public interface MemoRepository {
+    void getMemoList();
     void editMemo(int id, int password, String newPost);
 }
