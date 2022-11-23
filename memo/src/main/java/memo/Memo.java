d0771c716446cc5146c2afb30befc5305e1050d2
arkt0956 <55053131+arkt0956@users.noreply.github.com>
2022-11-23 12:51:24 +0900

    add compareTo

--- memo/src/main/java/memo/Memo.java
+++ memo/src/main/java/memo/Memo.java
@@ -1,4 +1,20 @@
 package memo;
 
-public class Memo {
+public class Memo implements Comparable<Memo> {
+    int id;
+    String name;
+    int password;
+    String post;
+    String date;
+    @Override
+    public int compareTo(Memo m) {
+        int comparedResult = this.date.compareTo(m.date)
+        if(comparedResult > 0) {
+            return 1;
+        }
+        else if(comparedResult < 0) {
+            return -1;
+        }
+        return 0;
+    }
 }
--- memo/src/main/java/memo/MemoList.java
+++ memo/src/main/java/memo/MemoList.java
@@ -1,5 +1,26 @@
 package memo;
 
+import java.util.Collections;
+
 public class MemoList {
-    static Memo[] memoList
+    static Memo[] memoList = new Memo[500]; //여러 건의 메모를 저장하는 클래스 이다.
+    static int count= 0;
+
+
+    // 메모 전체를 조회할 수 있다. (Getter/Setter 존재)
+    public Memo[] getMemo() {
+        return memoList;
+    }
+    // 작성 최신 순으로 메모를 출력한다.
+    public void printMemo() {
+        Collections.sort(memoList);
+        for(Memo memo : memoList) {
+            System.out.println(memo.id + "\t" + memo.post + "\t (" + memo.name
+                + ", " + memo.date);
+        }
+    }
+    // 1건의 글을 메모 리스트에 추가할 수 있다.
+    public void saveMemo(Memo memo) {
+        memoList[count++] = memo;
+    }
 }
